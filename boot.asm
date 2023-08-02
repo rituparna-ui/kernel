4dbe502da0939970e33a2cb799ea0e16234c60bc
Rituparna W <rituparnaw16@gmail.com>
2023-08-02 12:44:32 +0530

    Print string using loop

--- boot.asm
+++ boot.asm
@@ -1,13 +1,28 @@
 ORG 0x7C00        ; set origin to address 0x7C00
 BITS 16           ; set execution mode to 16 bits
 
-start:
-  mov ah, 0eh
-  mov al, 'A'
-  mov bx, 0
-  int 0x10        ; bios interrupt
-  
+start:      ; bios interrupt
+  mov si, message
+  call print
   jmp $
+
+print:
+  mov bx, 0
+.loop:
+  lodsb
+  cmp al, 0
+  je .done
+  call print_char
+  jmp .loop
+.done:
+  ret
+
+print_char:
+  mov ah, 0eh
+  int 0x10
+  ret
+
+message: db "Hello, Ritu !", 0
 
 times 510-($-$$) db 0
 dw 0xAA55
